d72b0c85b4764026d5b056f4aabe3d048184fdc9
malliina <malliina123@gmail.com>
2014-10-03 13:16:52 +0300

    added debian, startup necessities

---
+++ app/com/mle/pimpcloud/Starter.scala
@@ -0,0 +1,10 @@
+package com.mle.pimpcloud
+
+import com.mle.play.PlayLifeCycle
+
+/**
+ * @author Michael
+ */
+object Starter extends PlayLifeCycle {
+  override def appName: String = "pimpcloud"
+}

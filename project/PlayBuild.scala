b184ec5a5326be634717a264c7d051ef27347dc0
malliina <malliina123@gmail.com>
2014-10-09 20:51:05 +0300

    logconf fix

--- project/PlayBuild.scala
+++ project/PlayBuild.scala
@@ -8,7 +8,7 @@ object PlayBuild extends Build {
   lazy val p = PlayProjects.plainPlayProject("pimpcloud").settings(commonSettings: _*)
   val mleGroup = "com.github.malliina"
   val commonSettings = SbtNativePackager.packagerSettings ++ LinuxPlugin.debianSettings ++ Seq(
-    version := "0.1.1",
+    version := "0.1.2",
     scalaVersion := "2.11.2",
     retrieveManaged := false,
     fork in Test := true,

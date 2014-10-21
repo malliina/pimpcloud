fab94dd36536f85b10c09d0701be9afe2332000d
malliina <malliina123@gmail.com>
2014-10-21 15:32:39 +0300

    rem db dep

--- app/com/mle/db/DatabaseLike.scala
+++ app/com/mle/db/DatabaseLike.scala
@@ -1,63 +1,63 @@
-package com.mle.db
-
-import com.mle.util.Log
-
-import scala.slick.driver.H2Driver.simple._
-import scala.slick.jdbc.meta.MTable
-import scala.slick.jdbc.{GetResult, SetParameter, StaticQuery}
-import scala.slick.lifted.AbstractTable
-
-/**
- * @author Michael
- */
-trait DatabaseLike extends Log {
-  def database: Database
-
-  def tableQueries: Seq[TableQuery[_ <: Table[_]]]
-
-  /**
-   * Idempotent.
-   */
-  def init(): Unit = {
-    withSession(implicit session => {
-      log info s"Ensuring all tables exist..."
-      createIfNotExists(tableQueries: _*)
-    })
-  }
-
-  def withSession[T](f: Session => T) = database withSession f
-
-  def exists[T <: AbstractTable[_]](table: TableQuery[T])(implicit session: Session) =
-    MTable.getTables(table.baseTableRow.tableName).list(session).nonEmpty
-
-  def createIfNotExists[T <: Table[_]](tables: TableQuery[T]*)(implicit session: Session) =
-    tables.reverse.filter(t => !exists(t)(session)).foreach(t => initTable(t))
-
-  def initTable[T <: Table[_]](table: TableQuery[T])(implicit session: Session) = {
-    table.ddl.create(session)
-    val msg = s"Created table: ${table.baseTableRow.tableName}"
-    log info msg
-  }
-
-  def dropAll() = withSession(implicit s => {
-    tableQueries.foreach(t => {
-      if (exists(t)) {
-        executePlain(s"DROP TABLE ${t.baseTableRow.tableName}")
-        // ddl.drop fails if the table has a constraint to something nonexistent
-        //        t.ddl.drop
-      }
-      log info s"Dropped table: ${t.baseTableRow.tableName}"
-    })
-  })
-
-  def executePlain(queries: String*) =
-    withSession(s => queries.foreach(q => StaticQuery.updateNA(q).execute(s)))
-
-  def queryPlain[R](query: String)(implicit rconv: GetResult[R]) =
-    withSession(s => StaticQuery.queryNA[R](query).list(s))
-
-  def queryPlainParam[R, P](query: String, param: P)(implicit pconv: SetParameter[P], rconv: GetResult[R]) = {
-    val q = StaticQuery.query[P, R](query)
-    withSession(s => q(param).list(s))
-  }
-}
+//package com.mle.db
+//
+//import com.mle.util.Log
+//
+//import scala.slick.driver.H2Driver.simple._
+//import scala.slick.jdbc.meta.MTable
+//import scala.slick.jdbc.{GetResult, SetParameter, StaticQuery}
+//import scala.slick.lifted.AbstractTable
+//
+///**
+// * @author Michael
+// */
+//trait DatabaseLike extends Log {
+//  def database: Database
+//
+//  def tableQueries: Seq[TableQuery[_ <: Table[_]]]
+//
+//  /**
+//   * Idempotent.
+//   */
+//  def init(): Unit = {
+//    withSession(implicit session => {
+//      log info s"Ensuring all tables exist..."
+//      createIfNotExists(tableQueries: _*)
+//    })
+//  }
+//
+//  def withSession[T](f: Session => T) = database withSession f
+//
+//  def exists[T <: AbstractTable[_]](table: TableQuery[T])(implicit session: Session) =
+//    MTable.getTables(table.baseTableRow.tableName).list(session).nonEmpty
+//
+//  def createIfNotExists[T <: Table[_]](tables: TableQuery[T]*)(implicit session: Session) =
+//    tables.reverse.filter(t => !exists(t)(session)).foreach(t => initTable(t))
+//
+//  def initTable[T <: Table[_]](table: TableQuery[T])(implicit session: Session) = {
+//    table.ddl.create(session)
+//    val msg = s"Created table: ${table.baseTableRow.tableName}"
+//    log info msg
+//  }
+//
+//  def dropAll() = withSession(implicit s => {
+//    tableQueries.foreach(t => {
+//      if (exists(t)) {
+//        executePlain(s"DROP TABLE ${t.baseTableRow.tableName}")
+//        // ddl.drop fails if the table has a constraint to something nonexistent
+//        //        t.ddl.drop
+//      }
+//      log info s"Dropped table: ${t.baseTableRow.tableName}"
+//    })
+//  })
+//
+//  def executePlain(queries: String*) =
+//    withSession(s => queries.foreach(q => StaticQuery.updateNA(q).execute(s)))
+//
+//  def queryPlain[R](query: String)(implicit rconv: GetResult[R]) =
+//    withSession(s => StaticQuery.queryNA[R](query).list(s))
+//
+//  def queryPlainParam[R, P](query: String, param: P)(implicit pconv: SetParameter[P], rconv: GetResult[R]) = {
+//    val q = StaticQuery.query[P, R](query)
+//    withSession(s => q(param).list(s))
+//  }
+//}
--- project/PlayBuild.scala
+++ project/PlayBuild.scala
@@ -21,7 +21,7 @@ object PlayBuild extends Build {
       mleGroup %% "play-base" % "0.1.0",
       "org.java-websocket" % "Java-WebSocket" % "1.3.0",
 //      "com.h2database" % "h2" % "1.3.176",
-      "com.typesafe.slick" %% "slick" % "2.1.0",
+//      "com.typesafe.slick" %% "slick" % "2.1.0",
       play.PlayImport.filters,
       play.PlayImport.cache
     ),

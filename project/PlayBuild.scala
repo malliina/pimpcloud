373008e2b2b58f5d9f4085c7915251cc9662fac7
malliina <malliina123@gmail.com>
2014-10-23 18:48:10 +0300

    bugfix

--- project/PlayBuild.scala
+++ project/PlayBuild.scala
@@ -8,7 +8,7 @@ object PlayBuild extends Build {
   lazy val p = PlayProjects.plainPlayProject("pimpcloud").settings(commonSettings: _*)
   val mleGroup = "com.github.malliina"
   val commonSettings = SbtNativePackager.packagerSettings ++ LinuxPlugin.debianSettings ++ Seq(
-    version := "0.1.7",
+    version := "0.1.8",
     scalaVersion := "2.11.2",
     retrieveManaged := false,
     fork in Test := true,

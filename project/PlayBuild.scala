d6a6c4c671af8d548f288e955512eeb728e33701
Michael Skogberg <michael.skogberg@outlook.com>
2015-09-05 23:35:50 +0300

    Version upd

--- project/PlayBuild.scala
+++ project/PlayBuild.scala
@@ -17,7 +17,7 @@ object PlayBuild extends Build {
   val mleGroup = "com.github.malliina"
 
   val commonSettings = linuxSettings ++ Seq(
-    version := "0.4.1",
+    version := "0.4.2",
     scalaVersion := "2.11.7",
     retrieveManaged := false,
     fork in Test := true,

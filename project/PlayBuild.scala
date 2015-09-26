6ddc7fe649dd9561f8b3333b6ad63c05ecb28912
Michael Skogberg <michael.skogberg@outlook.com>
2015-09-26 12:23:42 +0300

    Update deps; configure prod logging

--- project/PlayBuild.scala
+++ project/PlayBuild.scala
@@ -6,6 +6,7 @@ import com.typesafe.sbt.SbtNativePackager._
 import com.typesafe.sbt.packager.{Keys => PackagerKeys}
 import play.routes.compiler.InjectedRoutesGenerator
 import play.sbt.PlayImport
+import play.sbt.PlayImport.PlayKeys
 import play.sbt.routes.RoutesKeys
 import sbt.Keys._
 import sbt._
@@ -17,7 +18,7 @@ object PlayBuild extends Build {
   val mleGroup = "com.github.malliina"
 
   val commonSettings = linuxSettings ++ Seq(
-    version := "0.4.3",
+    version := "0.4.4",
     scalaVersion := "2.11.7",
     retrieveManaged := false,
     fork in Test := true,
@@ -26,7 +27,7 @@ object PlayBuild extends Build {
       Resolver.bintrayRepo("malliina", "maven")
     ),
     libraryDependencies ++= Seq(
-      mleGroup %% "play-base" % "0.6.1",
+      mleGroup %% "play-base" % "2.3.0",
       "org.java-websocket" % "Java-WebSocket" % "1.3.0",
       PlayImport.filters,
       PlayImport.cache
@@ -37,7 +38,8 @@ object PlayBuild extends Build {
       "-Xlint:-options"
     ),
     scalacOptions += "-target:jvm-1.8",
-    RoutesKeys.routesGenerator := InjectedRoutesGenerator
+    RoutesKeys.routesGenerator := InjectedRoutesGenerator,
+    PlayKeys.externalizeResources := false
   )
 
   def linuxSettings = LinuxPlugin.playSettings ++ Seq(
@@ -50,7 +52,8 @@ object PlayBuild extends Build {
       val linuxName = (name in Linux).value
       Seq(
         s"-Dgoogle.oauth=/etc/$linuxName/google-oauth.key",
-        s"-Dlog.dir=/var/run/$linuxName/logs"
+        s"-Dlog.dir=/var/run/$linuxName/logs",
+        "-Dlogger.resource=prod-logger.xml"
       )
     },
     PackagerKeys.packageSummary in Linux := "This is the pimpcloud summary.",

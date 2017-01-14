bbaa92f7c0789871d36b0f3c33e82d96895c4b51
Michael Skogberg <malliina123@gmail.com>
2017-01-14 22:05:23 +0100

    Use HTTPS

--- app/controllers/AdminOAuth.scala
+++ app/controllers/AdminOAuth.scala
@@ -2,11 +2,15 @@ package controllers
 
 import akka.stream.Materializer
 import com.malliina.play.controllers.OAuthControl
-import play.api.mvc.Call
+import play.api.mvc.{Call, RequestHeader}
 
 class AdminOAuth(mat: Materializer) extends OAuthControl(mat) {
   // OAuth
   override val sessionUserKey: String = "email"
+
+  // temp hack
+  override def redirURL(request: RequestHeader): String =
+    oAuthRedir.absoluteURL(secure = true)(request)
 
   override def isAuthorized(email: String): Boolean = email == "malliina123@gmail.com"
 
